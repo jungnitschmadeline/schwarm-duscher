c368c36f37aabd113b0ce341179058c73e873ac2
Madeline <madeline.jungnitsch@haw-hamburg.de>
2020-01-27 08:08:23 +0100

    Updates

---
+++ precache-manifest.3158e89056129b60cd4c970dd66bc330.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e592ba3fd496f73d761786606a79d43c",
+    "url": "/schwarm-duscher/index.html"
+  },
+  {
+    "revision": "70f489c5a3b55db92e29",
+    "url": "/schwarm-duscher/static/css/main.1fa29a6b.chunk.css"
+  },
+  {
+    "revision": "0058cecb01986b5f59e9",
+    "url": "/schwarm-duscher/static/js/2.7356dc4b.chunk.js"
+  },
+  {
+    "revision": "5a3008287d4b18c4485762d3e15d1106",
+    "url": "/schwarm-duscher/static/js/2.7356dc4b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70f489c5a3b55db92e29",
+    "url": "/schwarm-duscher/static/js/main.9cd97c64.chunk.js"
+  },
+  {
+    "revision": "547ec07e9eafb8fdac5f",
+    "url": "/schwarm-duscher/static/js/runtime-main.827bb6a6.js"
+  },
+  {
+    "revision": "7dba0bbfdc24f51e43949f3213495d2a",
+    "url": "/schwarm-duscher/static/media/WWW.7dba0bbf.png"
+  },
+  {
+    "revision": "6d0bc75583360fd8f632f43f0be2980b",
+    "url": "/schwarm-duscher/static/media/gruppe.6d0bc755.svg"
+  },
+  {
+    "revision": "c754c5b734e2a4d39c245cb5f593e3bd",
+    "url": "/schwarm-duscher/static/media/gruppe.c754c5b7.svg"
+  }
+]);
